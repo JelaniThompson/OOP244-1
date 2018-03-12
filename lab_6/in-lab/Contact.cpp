7c1ac4043679f6eef2d34ae9250623ebdd607591
Lean Junio <leanjunio@live.com>
2018-03-12 15:55:35 -0400

    adding work from lab 6

--- lab_6/in-lab/Contact.cpp
+++ lab_6/in-lab/Contact.cpp
@@ -6,6 +6,20 @@ using namespace std;
 
 namespace sict
 {
+	// Checks if the passed phoneNumber is valid
+	bool Contact::isValidPhoneNumber(const long long phoneNumber)
+	{
+		string phoneNumberString = to_string(phoneNumber);
+		bool invalid = phoneNumber <= 0 || phoneNumberString.length() < 11 || phoneNumberString.length() > 12 || phoneNumberString[phoneNumberString.length() - 7] == '0' || phoneNumberString[phoneNumberString.length() - 10] == '0';
+		return !invalid;
+	}
+
+	bool Contact::isEmpty() const
+	{
+		bool empty = m_name[0] == '\0' && m_phoneNumbers == nullptr && m_noOfPhoneNumbers = 0;
+		return empty;
+	}
+
 	// Set to safe empty states
 	Contact::Contact()
 	{
@@ -14,16 +28,77 @@ namespace sict
 		m_phoneNumbers = nullptr;
 	}
 
+	// Gets the data when the object is created: name, phoneNumbers, number of phoneNumbers in the array
 	Contact::Contact(const char* name, const long long* phoneNumbers, const int noOfPhoneNumbers)
 	{
-		bool valid = name && phoneNumbers != nullptr && noOfPhoneNumbers > 0;
-
-		if (valid)
+		// Check if the incoming name is null or empty
+		bool isNotEmptyOrNull = name != nullptr && name[0] != '\0' && phoneNumbers != nullptr && noOfPhoneNumbers > 0;
+		if (isNotEmptyOrNull)
 		{
+			// Copy the passed data into m_name
 			strcpy(m_name, name);
+
+			// Allocate new memory
+			m_phoneNumbers = new long long[noOfPhoneNumbers];
+			m_noOfPhoneNumbers = 0;
+
+			// Check for valid numbers
+			for	(int i = 0; i < noOfPhoneNumbers; i++)
+			{
+				// If the current phoneNumbers element is valid
+				if (isValidPhoneNumber(phoneNumbers[i]))
+				{
+					// increase the m_noOfPhoneNumbers by 1
+					m_noOfPhoneNumbers += 1;
+
+					// Copy the valid phoneNumber into the m_phoneNumbers
+					m_phoneNumbers[m_noOfPhoneNumbers - 1] = phoneNumbers[i];
+				}
+			}
 		}
+		else
+		{
+			// Call the first constructor
+			*this = Contact();
+		}
+		
 	}
+
+	void Contact::display() const{
+		string countryCode;
+		string areaCode;
+		string numberCode1;
+		string numberCode2;
+		string phoneNumberString;
+		int phoneNumberStringLength;
+		if (!isEmpty()) {
+		
+			cout << m_name << endl;
+			for (int phone = 0; phone < m_noOfPhoneNumbers; phone++) {
+				phoneNumberString = to_string(m_phoneNumbers[phone]);
+				phoneNumberStringLength = phoneNumberString.length();
+				countryCode = phoneNumberString.substr(0, (phoneNumberStringLength - (areaCodeLength + numberLength)));
+				areaCode = phoneNumberString.substr(phoneNumberStringLength - (areaCodeLength + numberLength), areaCodeLength);
+				numberCode1 = phoneNumberString.substr(phoneNumberStringLength - numberLength, numberCode1Length);
+				numberCode2 = phoneNumberString.substr(phoneNumberStringLength - numberCode2Length, numberCode2Length);
+				
+				cout << "(+" + countryCode + ")" + " " + areaCode + " " + numberCode1 + "-" + numberCode2 << endl;			
+			}
+		}
+		else 
+		{
+			cout << "Empty contact!" << endl;
+		}
+}
+
 	Contact::~Contact()
 	{
+		// Deallocate the m_phoneNumbers
+		if (m_phoneNumbers != nullptr)
+		{
+			delete[] m_phoneNumbers;
+			m_phoneNumbers = nullptr;
+		}
 	}
+
 }
--- lab_6/in-lab/Contact.h
+++ lab_6/in-lab/Contact.h
@@ -1,7 +1,13 @@
 #ifndef SICT_CONTACT_H
 #define SICT_CONTACT_H
 
-namespace sict {
+namespace sict 
+{
+	const int areaCodeLength = 3;
+	const int numberLength = 7;
+	const int numberCode1Length = 3;
+	const int numberCode2Length = 4;
+
 	class Contact
 	{
 		// Stores the name of the contact
@@ -18,6 +24,7 @@ namespace sict {
 		~Contact();
 		void display() const;
 		bool isEmpty() const;
+	public:
 		Contact(const Contact& other) = delete;
 		Contact& operator=(const Contact& other) = delete;
 
